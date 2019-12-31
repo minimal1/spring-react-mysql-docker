71a9f8b2583b2c5e3e4bb3eeacc4e3d85f4f3d30
CheolminJeon <jcm940308@gmail.com>
2019-12-31 01:34:28 -0800

    UI

--- front-end-dev/react/src/components/Header.js
+++ front-end-dev/react/src/components/Header.js
@@ -8,58 +8,96 @@ class Header extends React.Component {
                     className="link"
                 >
                     <img
-                        className="icon-logo"
-                        src="/imgs/icon-twitter.png"
+                        className="header-logo"
+                        src="/imgs/icon-logo.JPEG"
                     />
                 </a>
+                
                 <form
-                    className="search-form"
+                    className="search-form mr-auto"
                 >
                     <input 
                         type="text"
                         name="keyword"
-                        placeholder="Search for papers"
+                        className="search-input"
+                        placeholder="Search"
                     />
-                    <button>
-                        Search
+                    <button
+                        className="icon"
+                    >
+                        <img
+                            src="/imgs/icon-search.png"
+                        />
                     </button>
                 </form>
-                <a
-                    className="link"
-                    href="http://127.0.0.1:8080"
+                
+                <nav
+                    className="header-nav"
                 >
-                    <img
-                        className="icon-twitter"
-                        src="/imgs/icon-twitter.png"
-                    />
-                    Twitter
-                </a>
-                <a
-                    className="link"
-                    href="http://127.0.0.1:8080/2"
-                >
-                    <img
-                        className="icon-slack"
-                        src="/imgs/icon-slack.png"
-                    />
-                    Slack
-                </a>
-                <a
-                    className="link"
-                >
-                    Trends
-                </a>
-                <a
-                    className="link"
-                >
-                    About
-                </a>
-                <a
-                    className="link"
-                >
-                    Log In/Register
-                </a>
+                    <ul
+                        className="header-nav list"
+                    >
+                        <li 
+                            className="list-item nav-link-right"
+                        >
+                            <a
+                                className="nav-link-right nav-textual-link"
+                                href="http://127.0.0.1:8080"
+                            >
+                                <img
+                                    className="nav-icon-twitter"
+                                    src="/imgs/icon-twitter.png"
+                                />
+                                Twitter
+                            </a>
+                        </li>
+                        <li 
+                            className="list-item nav-link-right"
+                        >
+                            <a
+                                className="nav-link-right nav-textual-link"
+                                href="http://127.0.0.1:8080"
+                            >
+                                <img
+                                    className="nav-icon-slack"
+                                    src="/imgs/icon-slack.png"
+                                />
+                                Slack
+                            </a>
+                        </li>
+                        <li 
+                            className="list-item nav-link-right"
+                        >
+                            <a
+                                className="nav-link-right nav-textual-link no-img"
+                                href="http://127.0.0.1:8080"
+                            >
+                                Trends
+                            </a>
+                        </li>
+                        <li 
+                            className="list-item nav-link-right"
+                        >
+                            <a
+                                className="nav-link-right nav-textual-link no-img"
+                                href="http://127.0.0.1:8080"
+                            >
+                                About
+                            </a>
+                        </li>
+                        <li 
+                            className="list-item nav-link-right"
+                        >
+                            <a
+                                className="nav-link-right nav-textual-link no-img"
+                                href="http://127.0.0.1:8080"
+                            >
+                                Log In/Register
+                            </a>
+                        </li>
 
+                    </ul>
+                </nav>
             </header>
         )
     }
