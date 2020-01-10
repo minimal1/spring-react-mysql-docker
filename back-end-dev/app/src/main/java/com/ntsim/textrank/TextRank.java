65d8ab4f3747562088249ec483b8782986cba4f9
NamJinWoo <cjsencks@naver.com>
2020-01-10 16:20:57 +0900

    separate sentences

--- back-end-dev/app/src/main/java/com/ntsim/textrank/TextRank.java
+++ back-end-dev/app/src/main/java/com/ntsim/textrank/TextRank.java
@@ -8,15 +8,22 @@ import org.apache.pdfbox.util.PDFTextStripper;
 public class TextRank {
 	public static void main(String[] args) throws IOException {
 
-        File myFile = new File("src/main/resources/test.pdf");
+        File myFile = new File("src/main/resources/paper.pdf");
 
         try (PDDocument doc = PDDocument.load(myFile)) {
 
             PDFTextStripper stripper = new PDFTextStripper();
+            stripper.setSortByPosition(true);
+            stripper.setStartPage(6);
+//            stripper.setEndPage(10);
+            stripper.setLineSeparator("");
             String text = stripper.getText(doc);
-
-            System.out.println("Text size: " + text.length() + " characters:");
-            System.out.println(text);
+            String simple = "[\\.?!]\\s+";      
+            String[] splitString = (text.split(simple));     
+            for (String string : splitString)   
+               System.out.println(string + "\n");
+//            System.out.println("Text size: " + text.length() + " characters:");
+//            System.out.println(text);
         }
     }
 }
