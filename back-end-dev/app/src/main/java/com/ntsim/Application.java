f877b1395e7e082ca8a4bc29f79b1467c3cff473
Hyungjin.Jeon <hyungjin.jeon@nanocellect.com>
2020-02-07 17:27:11 +0900

    aws account ignore

--- back-end-dev/app/src/main/java/com/ntsim/Application.java
+++ back-end-dev/app/src/main/java/com/ntsim/Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "classpath:aws.properties";
+	
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 	}
