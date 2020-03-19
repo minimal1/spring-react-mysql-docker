9bc3d2ac7f7f723f0f6e61fdfce5179b070d6259
NamJinWoo <cjsencks@naver.com>
2020-03-19 16:55:08 +0900

    request

---
+++ back-end-dev/app/src/main/java/com/ntsim/model/network/request/S3UploaderRequest.java
@@ -0,0 +1,19 @@
+package com.ntsim.model.network.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class S3UploaderRequest {
+	private String year;
+	
+	private String category;
+	
+	private String professor;
+}
