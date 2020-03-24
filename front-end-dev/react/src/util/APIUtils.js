786f0af12b534c29276700580504b688aa557153
minimal <jcm940308@gmail.com>
2020-03-24 16:32:42 +0900

    Update APIUtils.js

--- front-end-dev/react/src/util/APIUtils.js
+++ front-end-dev/react/src/util/APIUtils.js
@@ -2,36 +2,14 @@
 
 import { API_BASE_URL, ACCESS_TOKEN } from '../constants';
 
-const request = (options, contentType = 'application/json') => {
-  const headers = new Headers({
-    'Content-Type': contentType
-  });
+const request = (options, contentType) => {
+  const headers = new Headers();
 
-  if (localStorage.getItem(ACCESS_TOKEN)) {
-    headers.append('Authorization', localStorage.getItem(ACCESS_TOKEN));
+  if (contentType === 'JSON') {
+    headers.append('content-type', 'application/json');
+  } else if (contentType === 'formData') {
+    // pass
   }
-
-  const defaults = { headers: headers };
-  options = Object.assign({}, defaults, options);
-
-  return fetch(options.url, options).then(response =>
-    response.json().then(json => {
-      if (!response.ok) {
-        return Promise.reject(json);
-      }
-
-      if (json.result_code !== 'OK') {
-        return Promise.reject(json);
-      }
-      return json;
-    })
-  );
-};
-
-const request_file = options => {
-  const headers = new Headers({
-    'Content-Type': 'multipart/form-data'
-  });
 
   if (localStorage.getItem(ACCESS_TOKEN)) {
     headers.append('Authorization', localStorage.getItem(ACCESS_TOKEN));
@@ -59,35 +37,44 @@ export function getCurrentUser() {
     return Promise.reject('No access token set.');
   }
 
-  return request({
-    url: API_BASE_URL + '/user/me',
-    method: 'GET'
-  });
+  return request(
+    {
+      url: API_BASE_URL + '/user/me',
+      method: 'GET'
+    },
+    'JSON'
+  );
 }
 
 export function login(loginRequest) {
-  return request({
-    url: API_BASE_URL + '/login',
-    method: 'POST',
-    body: JSON.stringify(loginRequest)
-  });
+  return request(
+    {
+      url: API_BASE_URL + '/login',
+      method: 'POST',
+      body: JSON.stringify(loginRequest)
+    },
+    'JSON'
+  );
 }
 
 export function register(registerRequest) {
-  return request({
-    url: API_BASE_URL + '/register',
-    method: 'POST',
-    body: JSON.stringify(registerRequest)
-  });
+  return request(
+    {
+      url: API_BASE_URL + '/register',
+      method: 'POST',
+      body: JSON.stringify(registerRequest)
+    },
+    'JSON'
+  );
 }
 
-export function uploadFile(formData) {
+export function uploadFile(uploadRequest) {
   return request(
     {
       url: API_BASE_URL + '/upload_file',
       method: 'POST',
-      body: formData
+      body: uploadRequest
     },
-    'multipart/form-data'
+    'formData'
   );
 }
