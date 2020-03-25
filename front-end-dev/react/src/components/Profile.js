9b53cbde920e18dac52a2d0e7888885feb065a1d
minimal <jcm940308@gmail.com>
2020-03-25 13:27:54 +0900

    Orginazie

---
+++ front-end-dev/react/src/components/Profile.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React, { Component } from "react";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <p>Test</p>
+      </div>
+    );
+  }
+}
+
+export default Profile;
